8086a7adda1adb7944db40cf4513c60a6149ee4f
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-10 16:25:48 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter your name:");
+var username = Console.ReadLine();
+Console.WriteLine($"Hi {username}. Welcome to C#!");
