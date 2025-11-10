901f9f553073d4624284ebcfe0d37018922b1d78
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-10 16:55:04 +0700

    Add SampleApp.dll and refint/SampleApp.dll for .NET 9.0 build
    - Generated new assembly files for SampleApp targeting .NET 9.0.
    - Included necessary metadata and attributes in the assembly manifest.
    - Ensured compatibility with the .NET runtime and added security settings.

--- SampleApp/obj/Debug/net9.0/SampleApp.AssemblyInfo.cs
+++ SampleApp/obj/Debug/net9.0/SampleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8086a7adda1adb7944db40cf4513c60a6149ee4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
