e92d1ae2f4a3f27c7237ca2bcb154e37e2e72b26
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-20 00:33:12 +0700

    Delegate Anonymous methods

---
+++ LambdaDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LambdaDemo
+{
+    internal class Program
+    {
+        delegate string StringConverter(string value);
+        static void Main(string[] args)
+        {
+            StringConverter converter = delegate (string value)
+            {
+                Console.Write("Inline conversion to lowercase > ");
+                return value.ToLower();
+            };
+            var result = converter("This is some TEXT");
+            Console.WriteLine(result);
+        }
+    }
+}
