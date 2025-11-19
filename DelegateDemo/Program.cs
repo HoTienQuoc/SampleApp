2e55323fe46d510f2fd50a00edee33a179e8727d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-19 15:11:39 +0700

    Add DelegateDemo project with initial setup and .gitignore

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DelegateDemo
+{
+
+    internal class Program
+    {
+        public delegate string MyDelegate(string s);
+        public static string GetLength(string s)
+        {
+            return "Length of the string is: " + s.Length;
+        }
+
+        public static string CustomerCount(string customer)
+        {
+            return "Total Customers: 100";
+        }
+        static void Main(string[] args)
+        {
+
+            MyDelegate delegate1 = new MyDelegate(GetLength);
+            MyDelegate delegate2 = new MyDelegate(CustomerCount);
+
+            var strlen = delegate1("This is some text");
+            var count = delegate2("Robert Addison");
+
+            Console.WriteLine(strlen);
+            Console.WriteLine(count);
+        }
+    }
+}
