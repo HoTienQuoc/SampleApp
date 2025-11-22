b1aca50b52f2e0bbfdb6ac39ecf40543b5037159
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-22 22:42:20 +0700

    Add detailed explanation of value types and reference types in note.txt and update Program.cs to demonstrate their behavior

--- StructDemo/Program.cs
+++ StructDemo/Program.cs
@@ -8,6 +8,24 @@
             SampleClass demoClass = new(name: "Anna");
             Console.WriteLine(demoStruct.BuildHelloMsg());
             Console.WriteLine(demoClass.BuildHelloMsg());
+
+            //Value type có nghĩa là khi bạn sao chép một biến giá trị, 
+            //bạn sao chép toàn bộ dữ liệu của biến đó vào một vị trí bộ nhớ mới.
+            SampleStruct myStruct1 = new(name: "Mark");
+            SampleStruct myStruct2 = myStruct1;
+            myStruct2.Name = "David";
+            Console.WriteLine(myStruct1.BuildHelloMsg());
+            Console.WriteLine(myStruct2.BuildHelloMsg());
+
+
+
+            //Reference value có nghĩa là khi bạn sao chép một biến tham chiếu, 
+            //bạn chỉ sao chép con trỏ đến đối tượng trong bộ nhớ chứ không sao chép chính đối tượng đó.
+            SampleClass myClass1 = new(name: "Mark");
+            SampleClass myClass2 = myClass1;
+            myClass2.Name = "David";
+            Console.WriteLine(myClass1.BuildHelloMsg());
+            Console.WriteLine(myClass2.BuildHelloMsg());
         }
     }
 }
