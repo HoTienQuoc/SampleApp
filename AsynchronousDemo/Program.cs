273cb958ad1ac3e231b1e2d4bd254513692363e4
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-24 22:21:28 +0700

    Refactor Main method to execute file processing tasks concurrently and improve output formatting

--- AsynchronousDemo/Program.cs
+++ AsynchronousDemo/Program.cs
@@ -36,16 +36,33 @@ namespace AsynchronousDemo
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             Console.WriteLine("\n=== Before async task calls ===\n");
-            string filename1 = await ProcessFileAsync("file1.txt");
-            Console.WriteLine($"End {filename1} at {DateTime.Now}\n");
-            string filename2 = await ProcessFileAsync("file2.txt");
-            Console.WriteLine($"End {filename2} at {DateTime.Now}\n");
-            string filename3 = await ProcessFileAsync("file3.txt");
-            Console.WriteLine($"End {filename3} at {DateTime.Now}");
-            Console.WriteLine("\n=== After async tasks calls ===\n");
+            // string filename1 = await ProcessFileAsync("file1.txt");
+            // Console.WriteLine($"End {filename1} at {DateTime.Now}\n");
+            // string filename2 = await ProcessFileAsync("file2.txt");
+            // Console.WriteLine($"End {filename2} at {DateTime.Now}\n");
+            // string filename3 = await ProcessFileAsync("file3.txt");
+            // Console.WriteLine($"End {filename3} at {DateTime.Now}");
+            // Console.WriteLine("\n=== After async tasks calls ===\n");
+            // stopwatch.Stop();
+            // TimeSpan timeSpan = stopwatch.Elapsed;
+            // Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds\n");
+
+            Task<string> task1 = ProcessFileAsync("file1.txt");
+            Task<string> task2 = ProcessFileAsync("file2.txt");
+            Task<string> task3 = ProcessFileAsync("file3.txt");
+
+            Console.WriteLine("=== After async tasks calls ===\n");
+
+            string filename1 = await task1;
+            Console.WriteLine($"End {filename1} at {DateTime.Now}");
+            string filename2 = await task2;
+            Console.WriteLine($"End {filename2} at {DateTime.Now}");
+            string filename3 = await task3;
+            Console.WriteLine($"End {filename3} at {DateTime.Now}\n");
+
             stopwatch.Stop();
             TimeSpan timeSpan = stopwatch.Elapsed;
-            Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds\n");
+            Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds");
         }
     }
 }
