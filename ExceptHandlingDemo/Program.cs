61e49a4b96d7c89de437296d5dcbb4ba78df73b2
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-24 14:43:27 +0700

    Add ExceptHandlingDemo project with voltage check and exception handling logic

---
+++ ExceptHandlingDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ExceptHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                VoltageCheck(501);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Voltage too high - disconnecting power");
+                Console.WriteLine(ex.Message);
+                if (ex is ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                else if (ex is NullReferenceException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void VoltageCheck(int voltage)
+        {
+            if (voltage > 500)
+                throw new ArgumentOutOfRangeException(nameof(voltage),
+                "Voltage exceeds safety parameters.");
+            else
+                Console.WriteLine("Voltage is safe.");
+        }
+    }
+}
