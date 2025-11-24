4c05dc935c683e72e87442bfa2491afd602150ad
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-24 22:17:03 +0700

    Refactor Main method to use asynchronous file processing and clean up commented code

--- AsynchronousDemo/Program.cs
+++ AsynchronousDemo/Program.cs
@@ -3,33 +3,49 @@ namespace AsynchronousDemo
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            Console.WriteLine("\n=== Before async task calls ===\n");
-            string filename1 = ProcessFile("file1.txt");
-            Console.WriteLine($"End {filename1} at {DateTime.Now}\n");
-            string filename2 = ProcessFile("file2.txt");
-            Console.WriteLine($"End {filename2} at {DateTime.Now}\n");
-            string filename3 = ProcessFile("file3.txt");
-            Console.WriteLine($"End {filename3} at {DateTime.Now}\n");
-            Console.WriteLine("=== After async tasks calls ===\n");
-            stopwatch.Stop();
-            TimeSpan timeSpan = stopwatch.Elapsed;
-            Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds");
-        }
-        private static string ProcessFile(string filename)
-        {
-            Console.WriteLine($"Start {filename} at {DateTime.Now}");
-            Task.Delay(6000).Wait();
-            return filename;
-        }
+        // static void Main(string[] args)
+        // {
+        //     Stopwatch stopwatch = Stopwatch.StartNew();
+        //     Console.WriteLine("\n=== Before async task calls ===\n");
+        //     string filename1 = ProcessFile("file1.txt");
+        //     Console.WriteLine($"End {filename1} at {DateTime.Now}\n");
+        //     string filename2 = ProcessFile("file2.txt");
+        //     Console.WriteLine($"End {filename2} at {DateTime.Now}\n");
+        //     string filename3 = ProcessFile("file3.txt");
+        //     Console.WriteLine($"End {filename3} at {DateTime.Now}\n");
+        //     Console.WriteLine("=== After async tasks calls ===\n");
+        //     stopwatch.Stop();
+        //     TimeSpan timeSpan = stopwatch.Elapsed;
+        //     Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds");
+        // }
+        // private static string ProcessFile(string filename)
+        // {
+        //     Console.WriteLine($"Start {filename} at {DateTime.Now}");
+        //     Task.Delay(6000).Wait();
+        //     return filename;
+        // }
 
         private static async Task<string> ProcessFileAsync(string filename)
         {
             Console.WriteLine($"Start {filename} at {DateTime.Now}");
             await Task.Delay(6000);
             return filename;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Console.WriteLine("\n=== Before async task calls ===\n");
+            string filename1 = await ProcessFileAsync("file1.txt");
+            Console.WriteLine($"End {filename1} at {DateTime.Now}\n");
+            string filename2 = await ProcessFileAsync("file2.txt");
+            Console.WriteLine($"End {filename2} at {DateTime.Now}\n");
+            string filename3 = await ProcessFileAsync("file3.txt");
+            Console.WriteLine($"End {filename3} at {DateTime.Now}");
+            Console.WriteLine("\n=== After async tasks calls ===\n");
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            Console.WriteLine($"Elapsed time: {timeSpan.Seconds} seconds\n");
         }
     }
 }
